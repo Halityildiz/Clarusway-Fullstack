27bf358b80103b4ae0fc40fb4b2f6872fb69a51e
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-10-04 11:57:27 +0300

    dosya düzenleme yapıldı

---
+++ Clarusway-Frontend/JavaScript/30Eylül-java/app.js
@@ -0,0 +1,55 @@
+// var quantity;
+// console.log(quantity)
+
+// quantity= 3;
+// console.log(quantity);
+
+// var a = 5;
+// console.log(a);
+
+// var _ = 8;
+// console.log(_);
+
+
+// userFirstEntryPoint; lower camel case
+// userFirstEntryPoint; Upper camel case
+
+
+// var alert = 5;
+// console.log(alert);
+// console.log(typeof alert);
+
+// alert("Merhaba Cohort 9")
+
+// var a = 10;
+// console.log("a baslangıc :" , a);
+// {
+//     var b = 5;
+// }
+// console.log(a);
+// console.log(b);
+ 
+
+// let a = 23;
+// console.log(a);
+
+// a = 11;
+// console.log(a);
+
+
+// console.log(c);
+// var c = 23;
+// console.log(c);
+
+// {
+//     let a = 4;
+//     console.log(a);
+// }
+// {
+//     let a = 6;
+//     console.log(a);
+// }
+// console.log(a);
+
+const x = "Hello"
+
---
+++ Clarusway-Frontend/JavaScript/30Eylül-java/app2.js
@@ -0,0 +1,27 @@
+// var a = 5;
+// var b = 10.03;
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+// integerlerde maksimimu  15 digit
+
+
+// var a = 12348912345678
+// var b = 123111145678912345678
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+//console.log(Number.MAX_SAFE_INTEGER); //9007199254740891
+
+//console.log(0.1 + 0.2);
+
+// var x = 0.1 + 0.2;
+// console.log(x);
+// console.log(x.toFixed(2)); string
+// console.log(+x.toFixed(5)); number
+// console.log(x);
+
+
+var c = "Clarus"
