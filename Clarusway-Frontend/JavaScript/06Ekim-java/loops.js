d554fe26e5993a890571887c420e759127ea3e03
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-10-06 23:10:56 +0300

    ders çalışması eklendi

---
+++ Clarusway-Frontend/JavaScript/06Ekim-java/loops.js
@@ -0,0 +1,73 @@
+// let i = 0;
+// let sum = 0;
+// while (i < 11) {
+//     sum += i;
+//     console.log(sum);
+//     i++
+// }
+// console.log({i});
+// console.log({sum});
+
+
+// let i = 0;
+// let sum = 0;
+// while (i < 11) {
+//     sum += i;
+//     console.log(i);
+//     if (i == 8 ) break;
+//     i ++
+// }
+// console.log(sum);
+
+
+// let i = 12;
+// let sum = 0;
+// while (i < 11) {
+//     sum += i;
+//     console.log(i);
+//     i ++
+// }
+// console.log(sum);
+
+
+// let i = 5;
+// let sum = 0;
+// do{
+//     sum+= i;
+//     i++;
+// }
+// while(i<7);
+// console.log(sum);
+
+
+// let text = 'Clarusway';
+// for(let i= 0; i < text.length; i++){
+//     console.log(text[i]);
+// }
+
+// let sum = 0; 
+// for(let i = 0; i<=10; i ++){
+//     if(i %2 == 0){
+//         // sum += i;
+//     console.log(i);
+//     }
+// }
+
+
+for(let i = 0; i <100 ; i++){
+    if(i%15 == 0){
+      console.log("FizzBuzz")
+    }
+    else if(i%5 == 0){
+      console.log("Buzz")
+    }
+    else if (i%3 == 0){
+      console.log("Fizz")
+    }
+  }
+
+
+
+
+
+
