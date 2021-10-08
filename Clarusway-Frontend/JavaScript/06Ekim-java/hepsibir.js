3e5853c66397e3ec6868bad82a94e6d0864eb8b7
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-10-09 00:29:27 +0300

    işlem yapıldı

--- Clarusway-Frontend/JavaScript/06Ekim-java/hepsibir.js
+++ Clarusway-Frontend/JavaScript/06Ekim-java/hepsibir.js
@@ -118,17 +118,24 @@
 // reserve_txt("Cohort__9--FS Harveyli Cumalar");
 
 
-for(let i = 1; i <100 ; i++){
-    if(i%5 == 0 && i%3 == 0){
-      console.log(`${i} = FizzBuzz`)
-    }
-    else if(i%5 == 0){
-      console.log(`${i} = Buzz`)
-    }
-    else if (i%3 == 0){
-      console.log(`${i} = Fizz`)
-    }
-    if(i%5 != 0 && i%3 != 0){
-        console.log(`${i}`)
-    }
+// for(let i = 1; i <100 ; i++){
+//     if(i%5 == 0 && i%3 == 0){
+//       console.log(`${i} = FizzBuzz`)
+//     }
+//     else if(i%5 == 0){
+//       console.log(`${i} = Buzz`)
+//     }
+//     else if (i%3 == 0){
+//       console.log(`${i} = Fizz`)
+//     }
+//     if(i%5 != 0 && i%3 != 0){
+//         console.log(`${i}`)
+//     }
+// }
+
+let myword="Mustafa";
+let lastword="";
+for (i = myword.length-1; i >= 0; i--) {
+    lastword +=myword[i];    
 }
+console.log(lastword.toUpperCase());
