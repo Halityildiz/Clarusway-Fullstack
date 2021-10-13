0bbb805f80c5b38610652884327613ac9fdbb512
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-10-14 01:34:46 +0300

    indeks bulma ekleme yapıldı

--- Clarusway-Frontend/JavaScript/coding-challenges/assignmnets/indbulma.js
+++ Clarusway-Frontend/JavaScript/coding-challenges/assignmnets/indbulma.js
@@ -11,39 +11,39 @@
 
 let str = 'Lorem ipsum dolor adalet amet consectetur adipisicing elit. Cupiditate numquam, minima soluta beatae a quas aliquam ex distinctio exercitationem illo adalet autem quod vitae. Repellat quasi ab dolorum, doloremque laudantium unde cum doloribus! Officiis delectus nulla error suscipit debitis incidunt. Accusamus aliquid odio inventore adalet nemo ea qui quam neque non? Nisi voluptas alias tempore numquam harum voluptatum ab laborum ea ipsum officia quisquam, maxime fugit animi eaque soluta molestiae, eum adalet ipsam impedit corrupti dolorem odio, cumque labore? Quod odit, sunt a itaque quisquam officiis blanditiis at dolorem, repudiandae dignissimos ipsam voluptatem incidunt. Suscipit adalet sunt optio corrupti eveniet beatae consequatur, impedit laboriosam eos mollitia, exercitationem rerum ipsum? Harum quidem ut, ea error modi nesciunt iste dolorum voluptates voluptate a, sit aut';
 const myArr = str.split(" ");
-let strAdalet = 'adalet';
-let indexAdalet = [];
+let strAranan = prompt("aranacak kelimeyi giriniz");
+let indexAranan = [];
   for (let i = 0; i < myArr.length; i++) {
-    if (myArr[i] === strAdalet){
-      indexAdalet.push(i);    
+    if (myArr[i] === strAranan){
+      indexAranan.push(i);    
     }
   }
-console.log(indexAdalet);
+console.log(indexAranan);
 
 
-let str = 'Lorem? ipsum dolor adalet amet consectetur adipisicing elit? Cupiditate numquam, minima soluta be?atae a quas aliquam ex distinctio exercitationem illo adalet autem quod vitae. Repellat quasi ab dolorum, doloremque laudantium unde cum doloribus! Officiis delectus nulla error suscipit debitis incidunt. Accusamus? aliquid odio? inventore adalet nemo ea qui quam neque non? Nisi voluptas alias tempore numquam harum voluptatum ab laborum ea ipsum officia quisquam, maxime fugit animi eaque soluta molestiae, eum adalet ipsam impedit corrupti dolorem odio, cumque labore? Quod odit, sunt a itaque quisquam? officiis blandi?tiis at dolorem, repudiandae dignissimos ipsam voluptatem incidunt. Suscipit adalet sunt optio corrupti eveniet beatae consequatur, impedit laboriosam eos mollitia, exercitationem rerum ipsum? Harum quidem ut, ea error modi nesciunt iste dolorum voluptates voluptate a, sit aut';
-var character = '?';
-let indexSoru = [];
-var i = str.indexOf(character);
-while (i != -1) {
-  indexSoru.push(i);
-  i = str.indexOf(character, i + 1);
-}
-console.log(indexSoru);
+// let str = 'Lorem? ipsum dolor adalet amet consectetur adipisicing elit? Cupiditate numquam, minima soluta be?atae a quas aliquam ex distinctio exercitationem illo adalet autem quod vitae. Repellat quasi ab dolorum, doloremque laudantium unde cum doloribus! Officiis delectus nulla error suscipit debitis incidunt. Accusamus? aliquid odio? inventore adalet nemo ea qui quam neque non? Nisi voluptas alias tempore numquam harum voluptatum ab laborum ea ipsum officia quisquam, maxime fugit animi eaque soluta molestiae, eum adalet ipsam impedit corrupti dolorem odio, cumque labore? Quod odit, sunt a itaque quisquam? officiis blandi?tiis at dolorem, repudiandae dignissimos ipsam voluptatem incidunt. Suscipit adalet sunt optio corrupti eveniet beatae consequatur, impedit laboriosam eos mollitia, exercitationem rerum ipsum? Harum quidem ut, ea error modi nesciunt iste dolorum voluptates voluptate a, sit aut';
+// var character = '?';
+// let indexSoru = [];
+// var i = str.indexOf(character);
+// while (i != -1) {
+//   indexSoru.push(i);
+//   i = str.indexOf(character, i + 1);
+// }
+// console.log(indexSoru);
 
 
 
 
 
-let str = 'Lorem? ipsum dolor adalet amet consectetur adipisicing elit? Cupiditate numquam, minima soluta be?atae a quas aliquam ex distinctio exercitationem illo adalet autem quod vitae. Repellat quasi ab dolorum, doloremque laudantium unde cum doloribus! Officiis delectus nulla error suscipit debitis incidunt. Accusamus? aliquid odio? inventore adalet nemo ea qui quam neque non? Nisi voluptas alias tempore numquam harum voluptatum ab laborum ea ipsum officia quisquam, maxime fugit animi eaque soluta molestiae, eum adalet ipsam impedit corrupti dolorem odio, cumque labore? Quod odit, sunt a itaque quisquam? officiis blandi?tiis at dolorem, repudiandae dignissimos ipsam voluptatem incidunt. Suscipit adalet sunt optio corrupti eveniet beatae consequatur, impedit laboriosam eos mollitia, exercitationem rerum ipsum? Harum quidem ut, ea error modi nesciunt iste dolorum voluptates voluptate a, sit aut';
-let character = prompt("aranacak karakteri giriniz");
-let indexSoru = [];
-var i = str.indexOf(character);
-while (i != -1) {
-  indexSoru.push(i);
-  i = str.indexOf(character, i + 1);
-}
-console.log(indexSoru);
+// let str = 'Lorem? ipsum dolor adalet amet consectetur adipisicing elit? Cupiditate numquam, minima soluta be?atae a quas aliquam ex distinctio exercitationem illo adalet autem quod vitae. Repellat quasi ab dolorum, doloremque laudantium unde cum doloribus! Officiis delectus nulla error suscipit debitis incidunt. Accusamus? aliquid odio? inventore adalet nemo ea qui quam neque non? Nisi voluptas alias tempore numquam harum voluptatum ab laborum ea ipsum officia quisquam, maxime fugit animi eaque soluta molestiae, eum adalet ipsam impedit corrupti dolorem odio, cumque labore? Quod odit, sunt a itaque quisquam? officiis blandi?tiis at dolorem, repudiandae dignissimos ipsam voluptatem incidunt. Suscipit adalet sunt optio corrupti eveniet beatae consequatur, impedit laboriosam eos mollitia, exercitationem rerum ipsum? Harum quidem ut, ea error modi nesciunt iste dolorum voluptates voluptate a, sit aut';
+// let character = prompt("aranacak karakteri giriniz");
+// let indexSoru = [];
+// var i = str.indexOf(character);
+// while (i != -1) {
+//   indexSoru.push(i);
+//   i = str.indexOf(character, i + 1);
+// }
+// console.log(indexSoru);
 
 
 
