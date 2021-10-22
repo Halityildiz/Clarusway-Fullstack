1a52def9e0692ddf3f1e9433bfcb5d6ce5408efc
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-10-23 00:33:36 +0300

    çalışma eklendi

---
+++ Clarusway-Frontend/JavaScript/21Ekim-Events/index2.js
@@ -0,0 +1,38 @@
+// let navbarItem1 = document.getElementsByClassName("nav__link")[0].innerText;
+// console.log(navbarItem1);
+// let navbarItem1 = document.getElementsByClassName("nav__links")[0].children[0].innerText;
+// console.log(navbarItem1);
+// let navbarItem1 = document.getElementsByClassName("nav__links")[0].children[1].innerText;
+// console.log(navbarItem1);
+// let navbarItem1 = document.getElementsByClassName("nav__links")[0].children[2].innerText;
+// console.log(navbarItem1);
+// let navbarItem1 = document.getElementsByClassName("nav__links")[0].children[3].innerText;
+// console.log(navbarItem1);
+// let navbarItem1 = document.getElementsByClassName("nav__links")[0].firstElementChild.innerText;
+// console.log(navbarItem1);
+
+let h1 = document.querySelector("h1");
+// console.log(h1);
+let span = h1.querySelector(".highlight");
+span.style.color = "red";
+// span.classList.remove("highlight");
+// span.className = "highlight";
+// span.classList.add("highlight");
+// console.log(span);
+
+// let headerDiv = h1.parentElement.children;
+// console.log(headerDiv);
+
+// [... headerDiv].forEach(function(element) {
+//     if (element != h1) {
+//         element.style.transform = "scale(0.5)";
+//         element.style.color = "purple";     
+//     }
+// });
+
+// for (let index = 0; index < headerDiv.length; index++) {
+//     if (headerDiv[index] != h1) {
+//         headerDiv[index].style.transform = "scale(0.7)";
+//         headerDiv[index].style.color = "purple";        
+//     }   
+// }
---
+++ Clarusway-Frontend/JavaScript/21Ekim-Events/index3.js
@@ -0,0 +1,50 @@
+//Dom Events
+
+// window.onload = (e)=> {
+//     console.log("your page is loaded");
+//     // alert("your page was rendered");
+// }
+
+// window.addEventListener("load",(e) => {
+//     console.log("windowload eventlistener added");
+// });
+
+// window.addEventListener("DOMContentLoaded", function () {
+//     console.log("DOMContent was loaded");
+// });
+
+let navbarItem1 = document.getElementsByClassName("nav__links")[0].children[0].innerText;
+
+let navbarItem2 = document.getElementsByClassName("nav__links")[0].children[1];
+let navbarItem3 = document.getElementsByClassName("nav__links")[0].children[2];
+console.log(navbarItem1);
+
+navbarItem1.onclick = () =>{
+    alert("Paths clicked by old version");
+};
+
+navbarItem1.onclick = () =>{
+    alert("Paths clicked by old version"); 
+};
+
+navbarItem2.addEventListener("mouseover", ()=>{
+    navbarItem2.innerText =    "Over Me!";
+    navbarItem2.style.color = "green";
+});
+
+navbarItem2.addEventListener("mouseout", ()=>{
+    navbarItem2.innerText = "Instructors";
+    navbarItem2.style.color = "#444444";
+    navbarItem2.style.fontSize = "1.7rem";
+});
+
+const randomNumber = () => {
+   return Math.round(Math.random()*255);
+};
+const randomColor = ()=> {
+    return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+}
+console.log(randomColor);
+navbarItem3.addEventListener("click", ()=> {
+    navbarItem3.style.backgroundColor = randomColor();
+});
