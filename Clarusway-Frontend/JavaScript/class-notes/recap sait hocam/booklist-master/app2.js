b05c276e82b0aedebbc97c0b9cf52c6d69aa6ff6
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-11-01 19:15:47 +0300

    search eklendi

--- Clarusway-Frontend/JavaScript/class-notes/recap sait hocam/booklist-master/app2.js
+++ Clarusway-Frontend/JavaScript/class-notes/recap sait hocam/booklist-master/app2.js
@@ -11,6 +11,10 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#tauthor');
 const isbn = document.querySelector('#isbn');
 const form = document.querySelector('form');
+const bookList = document.querySelector('#book-list');
+const deleteBtn = document.querySelector('.btn-danger');
+const searchBox = document.querySelector('.search');
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -19,8 +23,51 @@ form.addEventListener('submit', e => {
     const bookIsbn = isbn.value;
     const book = createBook(bookTitle, bookAuthor, bookIsbn);
     console.log(book);
-    if (book) {
-        
+    if (!(book.title  && book.author && book.isbn)); {
+        alert('check the form fields');
+    }else{
+        bookList.innerHTML += ` <tr>
+                                                            <td>${book.title}</td>
+                                                            <td>${book.author}</td>
+                                                            <td>${book.isbn}</td>
+                                                            <td><button
+                                                            type = 'button'
+                                                            class = 'btn btn-danger'>Delete</button></td>
+                                                </tr>`
     }
+    // bookList.querySelectorAll('button')..forEach(button => {
+    //     button.addEventListener('click', (e)=>{
+    //         e.target.closest('tr').remove();
+    //     });
+    // });
 
-})
+    bookList.addEventListener('click', (e) =>{
+        if (e.target.classList.contains('btn-danger')) {
+            e.target.closest('tr').remove();
+        }
+    })
+    form.reset();
+});
+
+searchBox.addEventListener('click', e => {
+    const searchKey = searchBox.value.toLowerCase();
+    searchBook(searchKey);
+});
+
+const searchBook = key => {
+    const allBooks = bookList.querySelectorAll('tr');
+    console.log(allBooks);
+  
+    allBooks.forEach(book => {
+      console.log(!book.children[0].innerText.toLowerCase().includes(key));
+      if (!book.children[0].innerText.includes(key)) {
+        book.classList.add('hide');
+      }
+    });
+    allBooks.forEach(book => {
+      console.log(book.children[0].innerText.toLowerCase().includes(key));
+      if (book.children[0].innerText.includes(key)) {
+        book.classList.remove('hide');
+      }
+    });
+  };
