a849ac847248fcd32ad38cbd768d5db47bdf8a96
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-11-20 09:34:45 +0300

    tw7 eklendi

--- Clarusway-Frontend/Workshops/teamwork7.js
+++ Clarusway-Frontend/Workshops/teamwork7.js
@@ -14,7 +14,7 @@ console.log(
 
 const notifications = 1;
 console.log(
-  `You have ${notifications} notification${notifications !== 1 || "s"}`
+  `You have ${notifications} notification${notifications !== 1 && "s"}`
 );
 
 // Unfortunately, our short-circuit evaluation will not work as intended here: notifications !== 1 && 's' evaluates to false, meaning we will actually be logging You have 1 notificationfalse. If we want our snippet to work correctly, we could consider the conditional operator: ${notifications === 1 ? '' : 's'}.
