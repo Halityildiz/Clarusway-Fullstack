9becdc376411720d9c04dd77caf1b116d3d42094
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-03 18:34:42 +0300

    ders calismasi

---
+++ Clarusway-Frontend/React/ClassNotes/lesson-1/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ClassComp from "./components/classcomp";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ClassComp />
+        {/* Edit <code>src/App.js</code> and save to reload. */}
+      </header>
+    </div>
+  );
+}
+export default App;
