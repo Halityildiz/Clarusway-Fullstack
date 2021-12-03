b408352cbeb490fa5cdf8762169c52ed73f860d2
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-03 23:24:04 +0300

    ders eklendi

---
+++ Clarusway-Frontend/React/ClassNotes/lesson-1/src/components/funccomponent.js
@@ -0,0 +1,11 @@
+function FuncComp(myprops) {
+  const old = myprops.old;
+  return (
+    <div>
+      <h1>Welcome to React JS from functional component</h1>
+      <p> Age: {old}</p>
+    </div>
+  );
+}
+
+export default FuncComp;
