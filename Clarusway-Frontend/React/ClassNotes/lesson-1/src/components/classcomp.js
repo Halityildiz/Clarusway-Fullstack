1b9956205165320c918b2ab1f4fcf1a094778e24
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-04 09:38:16 +0300

    ders sonu ekleme

--- Clarusway-Frontend/React/ClassNotes/lesson-1/src/components/classcomp.js
+++ Clarusway-Frontend/React/ClassNotes/lesson-1/src/components/classcomp.js
@@ -1,13 +1,30 @@
 import React from "react";
 
 class ClassComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { student: "Yusuf", counter: 0 };
+  }
+  // state = { student: " Joseph" };
+  incCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  decCounter = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
+    const { old, car } = this.props;
     return (
       <div>
         {" "}
         <h1>Welcome to ReactJS from Class Component</h1>
-        <p>Age: {this.props.old} </p>
-        <p>Car: {this.props.car} </p>
+        <p>Age: {old} </p>
+        <p>Car: {car} </p>
+        <p>Student:{this.state.student} </p>
+        <p>Counter:{this.state.counter} </p>
+        <button onClick={this.inCounter}>Increase</button>
+        <button onClick={this.decCounter}>Decrement</button>
       </div>
     );
   }
--- Clarusway-Frontend/React/ClassNotes/lesson-1/src/components/funccomponent.js
+++ Clarusway-Frontend/React/ClassNotes/lesson-1/src/components/funccomponent.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 function FuncComp({ old, color }) {
+  const [counter, setCounter] = useState(0);
+  const incCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decCounter = () => {
+    setCounter(counter - 1);
+  };
   // const old = myprops.old;
   // const { old } = myprops;
   return (
     <div>
       <h1>Welcome to React JS from functional component</h1>
       <p> Age: {old}</p>
-      <p> Age: {old}</p>
+      {/* <p> Age: {old}</p> */}
       <p> Color: {color.toUpperCase()}</p>
-      <p> Age: {old}</p>
+      {/* <p> Age: {old}</p> */}
+      <p>Counter : {counter}</p>
+      <button onClick={incCounter}>Increase</button>
+      <button onClick={decCounter}>Decrease</button>
     </div>
   );
 }
