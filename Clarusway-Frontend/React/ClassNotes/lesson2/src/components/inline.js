12416b03b048a45d15609d6df6fa3650fbe63a5a
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-06 21:13:07 +0300

    ders calismasi

---
+++ Clarusway-Frontend/React/ClassNotes/lesson2/src/components/inline.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const myStyles = {
+  paragraf: {
+    color: "blue",
+    fontSize: "1.2rem",
+    fontWeight: "bolder",
+  },
+  secondDiv: {
+    backgroundColor: "#b0b0b0",
+    padding: "1rem 10px",
+  },
+};
+
+const Inline = () => {
+  return (
+    <div style={{ backgroundColor: "red", color: "#fff" }}>
+      <h1 style={{ backgroundColor: "yellow", color: "#222" }}>
+        Hello from inline component
+      </h1>
+      <p style={myStyles.paragraf}>Inline styling in react</p>
+      <div style={myStyles.secondDiv}>
+        <p>Second Div</p>
+      </div>
+    </div>
+  );
+};
+export default Inline;
