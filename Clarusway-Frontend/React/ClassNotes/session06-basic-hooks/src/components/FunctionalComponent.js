d322ad5d6d270dca0a6ca1c42b840d26df8317a5
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-09 20:31:12 +0300

    ders arasi

--- Clarusway-Frontend/React/ClassNotes/session06-basic-hooks/src/components/FunctionalComponent.js
+++ Clarusway-Frontend/React/ClassNotes/session06-basic-hooks/src/components/FunctionalComponent.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { useState } from 'react'
 const FunctionalComponent = () => {
+  const [count, setCount] = useState(0)
+  const [age, setAge] = useState(4)
+
+  const increase = () => setCount(count + 1)
+
+  // const increase = ()=>{
+  //   setCount(count+1)
+  // }
   return (
     <div className="function">
       <h2>Functional Component</h2>
+      <p>Count: {count}</p>
+      <button onClick={increase}>increase</button>
     </div>
   )
 }
