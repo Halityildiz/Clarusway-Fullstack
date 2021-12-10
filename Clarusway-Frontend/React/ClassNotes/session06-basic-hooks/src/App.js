b6a92e0bfbb1ad17a64e3595ff7251d1514c1fb4
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-10 03:05:57 +0300

    ders sonu

--- Clarusway-Frontend/React/ClassNotes/session06-basic-hooks/src/App.js
+++ Clarusway-Frontend/React/ClassNotes/session06-basic-hooks/src/App.js
@@ -1,19 +1,19 @@
-// import './index.css'
-import { useState } from 'react'
-import ClassComponent from './components/ClassComponent'
-import FunctionalComponent from './components/FunctionalComponent'
-// import Users from './components/Users'
+import './index.css'
+// import { useState } from 'react'
+// import ClassComponent from './components/ClassComponent'
+// import FunctionalComponent from './components/FunctionalComponent'
+import Users from './components/Users'
 
 function App() {
-  const [classComp, setClassComp] = useState(true)
-  const [funcComp, setFuncComp] = useState(true)
+  // const [classComp, setClassComp] = useState(true)
+  // const [funcComp, setFuncComp] = useState(true)
   return (
     <div className="App">
-      <button onClick={() => setClassComp(!classComp)}>Toggle Class Comp</button>
+      {/* <button onClick={() => setClassComp(!classComp)}>Toggle Class Comp</button>
       {classComp && <ClassComponent />}
       <button onClick={() => setFuncComp(!funcComp)}>Toggle Class Comp</button>
-      {funcComp && <FunctionalComponent />}
-      {/* <Users /> */}
+      {funcComp && <FunctionalComponent />} */}
+      <Users />
     </div>
   )
 }
