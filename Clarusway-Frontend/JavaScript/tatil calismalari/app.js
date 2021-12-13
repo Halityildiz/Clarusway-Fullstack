a85d9a14c0a30fe166bb7c654efb4500d57e6d16
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-13 21:21:01 +0300

    eklendi

--- Clarusway-Frontend/JavaScript/tatil calismalari/app.js
+++ Clarusway-Frontend/JavaScript/tatil calismalari/app.js
@@ -1,16 +1,34 @@
-// setTimeout- setInterval
+// // setTimeout- setInterval
 
-console.log(this);
-setTimeout(function () {
-  console.log("naber dostum nasilsin");
-}, 2000);
+// console.log(this);
+// setTimeout(function () {
+//   console.log("naber dostum nasilsin");
+// }, 2000);
 
-let i = 0;
-let value = setInterval(function () {
-  i++;
-  console.log("sayi:", i);
-}, 1000);
+// let i = 0;
+// let value = setInterval(function () {
+//   i++;
+//   console.log("sayi:", i);
+// }, 1000);
 
-document.getElementById("btn").addEventListener("click", function () {
-  clearInterval(value);
-});
+// document.getElementById("btn").addEventListener("click", function () {
+//   clearInterval(value);
+// });
+
+// let arr = [4, 3, 2];
+// let mapped = arr.map((a, b) => a * b);
+// console.log(mapped);
+
+var a = 3 === "3";
+console.log(a);
+
+// function logThis() {
+//   this.desc = "logger";
+//   console.log(this);
+// }
+// new logThis();
+
+// function sayHello() {
+//   console.log("hello");
+// }
+// console.log(sayHello.prototype);
