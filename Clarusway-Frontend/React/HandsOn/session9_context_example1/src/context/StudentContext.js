d365ef53f040ce0e282120901f750558ba1fc913
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-21 17:30:09 +0300

    v1

--- Clarusway-Frontend/React/HandsOn/session9_context_example1/src/context/StudentContext.js
+++ Clarusway-Frontend/React/HandsOn/session9_context_example1/src/context/StudentContext.js
@@ -1,3 +1,4 @@
 import { createContext } from "react";
-
+//! createContext metodu ile bir Context oluşturup sonraki kullanimlar için
+//! bunu export ettik.
 export const StudentContext = createContext();
--- Clarusway-Frontend/React/HandsOn/session9_context_example2/src/App.js
+++ Clarusway-Frontend/React/HandsOn/session9_context_example2/src/App.js
@@ -1,7 +1,13 @@
+import ShowUsers from "./pages/ShowUsers";
+import UserContextProvider from "./context/UserContextProvider";
+
 function App() {
   return (
     <div>
       <h1>CONTEXT API EXAMPLE!</h1>
+      <UserContextProvider>
+        <ShowUsers />
+      </UserContextProvider>
     </div>
   );
 }
