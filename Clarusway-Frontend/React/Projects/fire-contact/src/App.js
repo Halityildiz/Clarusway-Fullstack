c9ff12343212ecfa766c45267821a877d8f631ec
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-30 01:08:54 +0300

    v4

--- Clarusway-Frontend/React/Projects/fire-contact/src/App.js
+++ Clarusway-Frontend/React/Projects/fire-contact/src/App.js
@@ -1,20 +1,35 @@
-// import logo from "./logo.svg";
-import React, { useState } from "react";
-
+import { useState } from "react";
 import FormComponent from "./components/form/FormComponent";
 import Contacts from "./components/contacts/Contacts";
+import { addInfo, updateInfo } from "./utils/functions";
+import { ToastContainer } from "react-toastify";
+
+const initialValues = { username: "", phoneNumber: "", gender: "NO INFO" };
 
 function App() {
-  const initialValues = {
-    username: "",
-    phoneNumber: "",
-    gender: "No Info",
-  };
   const [info, setInfo] = useState(initialValues);
+
+  const handleFormSubmit = () => {
+    if (info.id) {
+      updateInfo(info);
+    } else {
+      addInfo(info);
+    }
+  };
+
+  const editHandler = (id, username, phoneNumber, gender) => {
+    setInfo({ id, username, phoneNumber, gender });
+  };
+
   return (
     <div className="App">
-      <FormComponent info={info} setInfo={setInfo} />
-      <Contacts />
+      <FormComponent
+        info={info}
+        setInfo={setInfo}
+        handleFormSubmit={handleFormSubmit}
+      />
+      <Contacts editHandler={editHandler} />
+      <ToastContainer />
     </div>
   );
 }
