18fac9fefe309d001c91ba86af9a0cbe713104ff
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2021-12-29 14:06:52 +0300

    v3

--- Clarusway-Frontend/React/Projects/fire-contact/src/utils/functions.js
+++ Clarusway-Frontend/React/Projects/fire-contact/src/utils/functions.js
@@ -1,5 +1,5 @@
 import firebase from "./firebase";
-import { getDatabase, set, ref, push } from "firebase/database";
+import { getDatabase, set, query, ref, push, onValue } from "firebase/database";
 import { useState, useEffect } from "react";
 
 export const addInfo = (info) => {
@@ -15,18 +15,20 @@ export const addInfo = (info) => {
 };
 
 export const useFetch = () => {
+  const [contactList, setContactList] = useState();
   const [isLoading, setIsLoading] = useState;
   useEffect(() => {
     setIsLoading(true);
     const db = getDatabase();
     const userRef = ref(db, "contact");
+    onValue(query(userRef), (snapshot) => {
+      const contacts = snapshot.val();
+      const contactArray = [];
+      for (let id in contacts) {
+        contactArray.push({ id, ...contacts[id] });
+      }
+      setContactList(contactArray);
+    });
   }, []);
-  return { isLoading };
-};
-
-export const deleteInfo = (id) => {
-  const db = getDatabase();
-  // const userRef = ref(db, 'contact');
-  remove(ref(db, "contact/" + id));
-  successNote("Deleted");
+  return { isLoading, contactList };
 };
