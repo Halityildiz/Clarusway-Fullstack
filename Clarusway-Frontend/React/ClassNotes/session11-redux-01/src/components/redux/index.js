0a72be4030bb9743e48662a98253c57762257370
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-06 19:51:08 +0300

    v1

--- Clarusway-Frontend/React/ClassNotes/session11-redux-01/src/components/redux/index.js
+++ Clarusway-Frontend/React/ClassNotes/session11-redux-01/src/components/redux/index.js
@@ -1,22 +1,27 @@
-const initialState = {
-  counter: 0,
-};
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        counter: state.counter + 1,
-      };
-    case "DECREMENT":
-      return {
-        counter: state.counter - 1,
-      };
-    case "RESET":
-      return {
-        counter: 0,
-      };
-    default:
-      return state;
-  }
-};
-export default reducer;
+
+
+
+
+
+// const initialState = {
+//   counter: 0,
+// };
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return {
+//         counter: state.counter + 1,
+//       };
+//     case "DECREMENT":
+//       return {
+//         counter: state.counter - 1,
+//       };
+//     case "RESET":
+//       return {
+//         counter: 0,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+// export default reducer;
