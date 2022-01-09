88c56e3e7612c471e1aa94e8cd2c4b3bfabff14d
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-09 23:52:57 +0300

    v5

--- Clarusway-Frontend/React/ClassNotes/session13/src/components/Header.js
+++ Clarusway-Frontend/React/ClassNotes/session13/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = React.memo(({ img }) => {
       <img
         src={img ? img : cw}
         alt="CW_logo"
-        style={{ margin: "2rem", maxHeight: "200px" }}
+        style={{ margin: "3rem", maxHeight: "200px" }}
       />
     </div>
   );
