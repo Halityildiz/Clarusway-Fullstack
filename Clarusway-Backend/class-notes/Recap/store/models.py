6a579aaaa3efded05d66fa99f729362a4c5564fb
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-25 14:44:29 +0300

    v3

--- Clarusway-Backend/class-notes/Recap/store/models.py
+++ Clarusway-Backend/class-notes/Recap/store/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
   name = models.CharField(max_length = 50)
-  # isActive = models.BooleanField()
+  isActive = models.BooleanField()
+  
+  
+
+    
