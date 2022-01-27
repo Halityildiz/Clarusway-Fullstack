ee55db2bb7eac630d62dae46caff3dd50b5d6e70
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-27 22:21:31 +0300

    v4

--- Clarusway-Backend/class-notes/27 Ocak/student/views.py
+++ Clarusway-Backend/class-notes/27 Ocak/student/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .forms import StudentForm
+from .models import Student
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'student/index.html')
+
+# def student_page(request):
+#     return render(request,'student/student.html')
+
+
+def student_page(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            student_data = {
+                "first_name": form.cleaned_data.get('first_name'),
+                "last_name": form.cleaned_data.get('last_name'),
+                "number": form.cleaned_data.get('number'),
+                "profile_pic": form.cleaned_data.get('profile_image'),
+            }
+            student = Student(**student_data)
+            student.save()
+            return redirect('student')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'student/student.html', context)
