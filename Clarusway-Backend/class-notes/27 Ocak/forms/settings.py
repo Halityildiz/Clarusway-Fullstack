2afecfee8d0fb18e4c34dab6885a5445c35fa25f
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-27 21:29:09 +0300

    v3

--- Clarusway-Backend/class-notes/27 Ocak/forms/settings.py
+++ Clarusway-Backend/class-notes/27 Ocak/forms/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bk7fah3nj4e8d#h3#w4c6jct8*1+!)5$!btezgx$31q&hda4wy'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student',
 ]
 
 MIDDLEWARE = [
