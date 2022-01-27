d59c347c3d1bfcbfda3f489c82bd810e7707fcc7
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-28 00:22:00 +0300

    v6

--- Clarusway-Backend/class-notes/27 Ocak/student/views.py
+++ Clarusway-Backend/class-notes/27 Ocak/student/views.py
@@ -10,19 +10,32 @@ def index(request):
 #     return render(request,'student/student.html')
 
 
+# def student_page(request):
+#     form = StudentForm()
+#     if request.method == 'POST':
+#         form = StudentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             student_data = {
+#                 "first_name": form.cleaned_data.get('first_name'),
+#                 "last_name": form.cleaned_data.get('last_name'),
+#                 "number": form.cleaned_data.get('number'),
+#                 "profile_pic": form.cleaned_data.get('profile_image'),
+#             }
+#             student = Student(**student_data)
+#             student.save()
+#             return redirect('student')
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'student/student.html', context)
+
 def student_page(request):
     form = StudentForm()
     if request.method == 'POST':
         form = StudentForm(request.POST, request.FILES)
         if form.is_valid():
-            student_data = {
-                "first_name": form.cleaned_data.get('first_name'),
-                "last_name": form.cleaned_data.get('last_name'),
-                "number": form.cleaned_data.get('number'),
-                "profile_pic": form.cleaned_data.get('profile_image'),
-            }
-            student = Student(**student_data)
-            student.save()
+            form.save()
             return redirect('student')
 
     context = {
