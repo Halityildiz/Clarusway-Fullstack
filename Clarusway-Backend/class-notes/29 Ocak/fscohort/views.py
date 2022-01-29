d5630599145ecc9ee5c5662940f1407017e040f6
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-29 12:14:49 +0300

    v3

--- Clarusway-Backend/class-notes/29 Ocak/fscohort/views.py
+++ Clarusway-Backend/class-notes/29 Ocak/fscohort/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# def home_page(request):
+#     return HttpResponse("we are developers")
+
+def home_page(request):
+    return render(request, "fscohort/home.html")
