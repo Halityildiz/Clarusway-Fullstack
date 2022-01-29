e0a7793537dc8c9c3d2deeca71fa4dba740309e9
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-01-29 13:24:44 +0300

    v7

--- Clarusway-Backend/class-notes/29 Ocak/fscohort/urls.py
+++ Clarusway-Backend/class-notes/29 Ocak/fscohort/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
+
+   
 from django.urls import path
+from .views import home_page, student_list, student_add
 
 urlpatterns = [
-    path('', home_page name= 'home'),
+    path('', home_page, name="home"),
+    path('student_list/', student_list, name="list"),
+    path('student_add/', student_add, name="add"),
+
 ]
