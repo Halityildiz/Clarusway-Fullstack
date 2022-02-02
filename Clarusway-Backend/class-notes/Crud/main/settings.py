e7020dfcbe849ead3aa4e43c159a56d1f42d434a
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-02-02 03:45:10 +0300

    v12

--- Clarusway-Backend/class-notes/Crud/main/settings.py
+++ Clarusway-Backend/class-notes/Crud/main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-55x9v!v$gbt362&zy$vbp5)bs1#582965@98a%#ndumdr#5&om"
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config ("Debug")
+DEBUG = "True"
 
 ALLOWED_HOSTS = []
 
--- Clarusway-Backend/class-notes/Crud/todo/views.py
+++ Clarusway-Backend/class-notes/Crud/todo/views.py
@@ -1,5 +1,3 @@
-
-from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
