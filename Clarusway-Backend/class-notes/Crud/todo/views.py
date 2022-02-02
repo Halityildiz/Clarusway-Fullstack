94969adc39b53921652afbb7eb6f0fdb82cfd955
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-02-02 03:04:50 +0300

    vf

--- Clarusway-Backend/class-notes/Crud/todo/views.py
+++ Clarusway-Backend/class-notes/Crud/todo/views.py
@@ -45,4 +45,14 @@ def todo_update (request, id):
   return render (request, "todo/todo_update.html", context)
   
 def todo_delete (request, id):
-  pass
+  todo = Todo.objects.get(id=id)
+  
+  if request.method == "POST":
+    todo.delete()
+    return redirect("list")
+    
+    context = {
+      "todo":todo
+    }
+    
+    return render(request, "todo/todo_delete.html", context)
