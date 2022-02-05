07024a6a91b8f26eeca6c1cff434dfca3f2d8df3
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-02-06 00:49:41 +0300

    v6

--- Clarusway-Backend/class-notes/Authentication/user_example/views.py
+++ Clarusway-Backend/class-notes/Authentication/user_example/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def home(request):
   return render( request, 'user_example/home.html')
 
 def register(request):
-  form = UserCreationForm()
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      
+      username = form.cleaned_data.get("username")
+      password = form.cleaned_data.get("password1")
+      
+      user = authenticate(username = username, password= password)
+      
+      login(request, user)
+      return redirect("home")
+    else:
+      form = UserCreationForm()
   context = {
     'form':form
   }
