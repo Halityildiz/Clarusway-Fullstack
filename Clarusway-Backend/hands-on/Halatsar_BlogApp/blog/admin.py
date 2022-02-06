28bacf0b180774e8b9356f279379b671393e95dc
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-02-06 19:33:11 +0300

    v2

--- Clarusway-Backend/hands-on/Halatsar_BlogApp/blog/admin.py
+++ Clarusway-Backend/hands-on/Halatsar_BlogApp/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Category, Post, Like, PostView, Comment
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(PostView)
+admin.site.register(Comment)
