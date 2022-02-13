d86a138f9710819f99fddf8a3d70e43954f80d2b
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-02-13 08:04:26 +0300

    g3

--- Clarusway-Backend/class-notes/ClassBased/fscohort/urls.py
+++ Clarusway-Backend/class-notes/ClassBased/fscohort/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     # path('', home, name= "home"),
     path("", HomeView.as_view(), name = "home"),
     # path("student_list/", student_list, name= "list"),
-    # path("student_list/", StudentListView.as_view(), name= "list"),
+    path("student_list/", StudentListView.as_view(), name= "list"),
     # path("student_add/", student_add, name= "add"),
     path("student_add/", StudentCreateView.as_view(), name= "add"),
     # path("detail/<int:id>", student_detail, name= "detail"),
-    # path("detail/<int:id>", StudentDetailView.as_view(), name= "detail"),
+    path("detail/<int:id>", StudentDetailView.as_view(), name= "detail"),
     path("update/<int:id>", student_update, name= "update"),
     path("delete/<int:id>", student_delete, name= "delete")
 ] 
