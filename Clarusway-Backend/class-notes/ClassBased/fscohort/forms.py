a6b40b502ed5094b44712cf3f918635615b85bee
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-02-16 12:27:42 +0300

    gr

--- Clarusway-Backend/class-notes/ClassBased/fscohort/forms.py
+++ Clarusway-Backend/class-notes/ClassBased/fscohort/forms.py
@@ -3,7 +3,7 @@ from django.forms import fields
 from .models import Student
 
 
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = "__all__"
+# class StudentForm(forms.ModelForm):
+#     class Meta:
+#         model = Student
+#         fields = "__all__"
