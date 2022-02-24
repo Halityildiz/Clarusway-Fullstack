07f5b85757444f20947ad2426b8caae9f257e534
Halit YILDIZ <86552701+Halityildiz@users.noreply.github.com>
2022-02-25 01:10:52 +0300

    ve3

--- Clarusway-Backend/class-notes/drf/main/settings.py
+++ Clarusway-Backend/class-notes/drf/main/settings.py
@@ -44,15 +44,15 @@ ALLOWED_HOSTS = []
 #     "apiTodo",
 # ]
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+# MIDDLEWARE = [
+#     "django.middleware.security.SecurityMiddleware",
+#     "django.contrib.sessions.middleware.SessionMiddleware",
+#     "django.middleware.common.CommonMiddleware",
+#     "django.middleware.csrf.CsrfViewMiddleware",
+#     "django.contrib.auth.middleware.AuthenticationMiddleware",
+#     "django.contrib.messages.middleware.MessageMiddleware",
+#     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+# ]
 
 ROOT_URLCONF = "main.urls"
 
